6ecebff6ef5b2a4e678a0b0de0c91790cbd0ea24
kokkumong <jeongkh2001@gmail.com>
2025-09-18 21:05:07 +0900

    Login - Port 구현

---
+++ src/main/java/com/mysite/demo/domain/port/in/RegisterMemberUseCase.java
@@ -0,0 +1,17 @@
+package com.mysite.demo.domain.port.in;
+
+import com.mysite.demo.domain.model.Member;
+
+public interface RegisterMemberUseCase {
+    Member registerMember(RegisterMemberCommand command);
+    record RegisterMemberCommand(String email, String password, String userName){
+        public RegisterMemberCommand {
+            if(email == null || email.isBlank()){
+                throw new IllegalArgumentException("이메일은 필수 항목입니다.");
+            }
+            if(password == null || password.isBlank()){
+                throw new IllegalArgumentException("비밀번호는 필수 항목입니다.");
+            }
+        }
+    }
+}
