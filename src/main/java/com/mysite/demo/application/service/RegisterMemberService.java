fd397b463073e95eb5c6588e9a2f694a7401dfd3
kokkumong <jeongkh2001@gmail.com>
2025-09-18 21:30:46 +0900

    Login - Service구현

---
+++ src/main/java/com/mysite/demo/application/service/RegisterMemberService.java
@@ -0,0 +1,27 @@
+package com.mysite.demo.application.service;
+import com.mysite.demo.domain.model.Member;
+import com.mysite.demo.domain.port.in.RegisterMemberUseCase;
+import com.mysite.demo.domain.port.out.MemberPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RegisterMemberService implements RegisterMemberUseCase {
+    private final MemberPort memberPort;
+    @Override
+    public Member registerMember(RegisterMemberCommand command){
+        memberPort.findByEmail(command.email()).ifPresent(member -> {
+            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+        });
+
+        Member newMember = Member.withoutId(
+                command.email(),
+                command.password(),
+                command.userName()
+        );
+        return memberPort.save(newMember);
+    }
+}
